ffb01d9b78730f66a493a98e5c95aa6018bb7c59
dulara <it21014840@my.sliit.lk>
2023-01-30 22:20:57 +0530

    4th commit

---
+++ src/main/java/com/example/sms/config/SecurityConfiguration.java
@@ -0,0 +1,5 @@
+package com.example.sms.config;
+
+public class SecurityConfiguration {
+
+}
--- src/main/java/com/example/sms/entity/User.java
+++ src/main/java/com/example/sms/entity/User.java
@@ -30,12 +30,17 @@ public class User {
 	private String lastName;
 
 	private String email;
+
 	private String password;
 
 	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
 
 	private Collection<Role> roles;
+
+	public User() {
+
+	}
 
 	public User(String firstName, String lastName, String email, String password, Collection<Role> roles) {
 		super();
